8a0b5e0925613f3c6fe37caea2a3c7a6b08bb6da
Georgios Roumpie <giwrgosrubi@gmail.com>
2023-06-13 11:10:45 +0300

    [Remove] model eval not used

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ from src.image_defusion_model import ImageDiffusionModel
 device = 'cpu'
 
 # Load autoencoder
-vae = AutoencoderKL.from_pretrained('CompVis/stable-diffusion-v1-4',
-                                    subfolder='vae').to(device)
+vae = AutoencoderKL.from_pretrained('CompVis/stable-diffusion-v1-4', subfolder='vae').to(device)
 # Load tokenizer and the text encoder
 tokenizer = CLIPTokenizer.from_pretrained('openai/clip-vit-large-patch14')
 text_encoder = CLIPTextModel.from_pretrained('openai/clip-vit-large-patch14').to(device)
 # Load UNet model
 unet = UNet2DConditionModel.from_pretrained('CompVis/stable-diffusion-v1-4', subfolder='unet').to(device)
-unet.eval()
 # Load schedulers
 scheduler_LMS = LMSDiscreteScheduler(beta_start=0.00085,
                                      beta_end=0.012,
